81279c506e3b346ac670c48799daefa83796aa9d
Almaz Kunpeissov <almaz.kun@gmail.com>
2018-11-18 16:18:50 +0600

    Model\class\author
    Creating django backend

--- visitkoreakz/polls/models.py
+++ visitkoreakz/polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL)
--- visitkoreakz/visitkoreakz/urls.py
+++ visitkoreakz/visitkoreakz/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-]
+    path('polls/', include('polls.urls')),
+    path('', RedirectView.as_view(url='/polls/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
