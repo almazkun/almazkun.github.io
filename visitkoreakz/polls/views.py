8aba3c28ece4af693e8bffd8b88db42b278756b0
Almaz Kunpeissov <almaz.kun@gmail.com>
2018-11-24 21:21:29 +0600

    Auth set up

--- visitkoreakz/polls/views.py
+++ visitkoreakz/polls/views.py
@@ -46,3 +46,22 @@ class AuthorDetailView(generic.DetailView):
     def author_detail_view(request, primary_key):
         author = get_object_or_404(Author, pk=primary_key)
         return render(request, 'polls/author_detail.html', context={'author': author})
+
+
+def author_detail(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    return render(request, 'author_detail.html', context=context)
--- visitkoreakz/visitkoreakz/urls.py
+++ visitkoreakz/visitkoreakz/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     path('', RedirectView.as_view(url='/polls/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
