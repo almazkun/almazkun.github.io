9d1db7a9ff1e6f75031e182a1265052d6fe78814
Almaz Kunpeissov <almaz.kun@gmail.com>
2018-12-08 22:28:34 +0600

    Index file fix

--- visitkoreakz/polls/urls.py
+++ visitkoreakz/polls/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('book/', views.BookListView.as_view(), name='book'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('author/', views.AuthorListView.as_view(), name='author'),
-    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail')
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail '
+                                                                   '')
 ]
