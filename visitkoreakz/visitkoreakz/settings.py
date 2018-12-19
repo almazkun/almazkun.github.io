038eb7157ecb1e9250c71b5c818910f827fb3900
Almaz Kunpeissov <almaz.kun@gmail.com>
2018-12-19 11:16:32 +0600

    updaiting .gitignore

--- visitkoreakz/visitkoreakz/settings.py
+++ visitkoreakz/visitkoreakz/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9b&5r%qwhipzj+mj=t#te$97=i4n#%w!olg+q!crrbqj45c50+'
+# SECRET_KEY = '9b&5r%qwhipzj+mj=t#te$97=i4n#%w!olg+q!crrbqj45c50+'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
